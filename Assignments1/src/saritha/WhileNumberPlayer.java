e86f88200ca407c54b73cc47c7b7fa2299dda768
sarit <sarit@192.168.0.15>
2020-03-18 18:57:14 -0400

    Using While

---
+++ Assignments1/src/saritha/WhileNumberPlayer.java
@@ -0,0 +1,35 @@
+package saritha;
+
+public class WhileNumberPlayer {
+	private int limit;
+	
+	
+	public WhileNumberPlayer(int limit) {
+		this.limit =limit;
+	}
+	
+	
+
+	public void  printSquaresToLimit(){
+		int i=1;
+		while (i*i<limit) {
+			
+				System.out.println(i*i+" ");
+				i++;
+			}
+			
+		}
+	
+public void printcubesToLimit() {
+	int i=1;
+	while((i*i*i)<limit)
+	{
+		System.out.println(i*i*i);
+		i++;
+	}}
+	}
+	
+
+	
+
+
---
+++ Assignments1/src/saritha/Whilerunner.java
@@ -0,0 +1,13 @@
+package saritha;
+
+public class Whilerunner {
+
+	public static void main(String[] args) {
+		WhileNumberPlayer player =new WhileNumberPlayer(30);
+		player.printSquaresToLimit();
+		player.printcubesToLimit();
+		
+
+	}
+
+}
